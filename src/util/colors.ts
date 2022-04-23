20a9f1e54888d8484d7d6496c137af8277ba45d3
FranciscoGuedes <francisco.guedes@polarising.com>
2022-04-23 18:35:51 +0100

    added assets images background and colors util

---
+++ src/util/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  background: {
+    yellow: '#ffd42d',
+    blue: '#1544c0',
+    orange: '#f86449',
+    purple: '#5210ac',
+  },
+  fade: '#fffade',
+  lettersForBlue: '#96ffca',
+  lettersForOrange: '#fff55a',
+  letterForPurple: '#fbe10f',
+}
