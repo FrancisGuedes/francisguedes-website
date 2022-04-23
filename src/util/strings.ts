2ae2a4eba8e2a80c070c6badd61a688d0236fcc1
FranciscoGuedes <francisco.guedes@polarising.com>
2022-04-23 18:15:40 +0100

    added strings and var env

---
+++ src/util/strings.ts
@@ -0,0 +1,18 @@
+export const functionalitiesAlias = {
+  cv: "résumé",
+  work: "work",
+  playground: "playground",
+}
+
+export const strings = {
+  heroPage: {
+    introText: {
+      firstParagraph: "Hello, I'm Francis.\nA multi-skilled Software Engineer.\nSpecialized in ",
+      secondParagraph: "\nGreat hand-stander and former professional chef.",
+      endPoint: ".",
+      dynamicWords: "front-end back-end mobile development decentralized applications",
+    },
+    talk:"let's talk?"
+  }
+}
+
