3c9854fa629039a6166e4e5f2589ef4434893fa7
FranciscoGuedes <francisco.guedes@polarising.com>
2022-05-14 23:53:04 +0100

    added carousel component

--- src/containers/playground/playground.tsx
+++ src/containers/playground/playground.tsx
@@ -1,178 +1,25 @@
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import ArrowBack from '../../components/arrow-back/arrowBack';
 import { strings } from '../../util/strings';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, A11y, EffectCards } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
-
 import { Transition, Variants } from '../../util/animations/slidePageVariables';
 import './playground.css';
 import SocialMedia from '../../components/social-media/socialMedia';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import HrLine from '../../components/hr-line/hrLine';
+import Carousel from '../../components/carousel/carousel';
+import { useCallback, useEffect, useState } from 'react';
 
 const Playground = () => {
-  const [toggleArrowStatus, setStatus] = useState<boolean[]>([]);
-  const [toggleCarouselStatus, setToggleCarouselStatus] = useState<boolean[]>([]);
-  const [toggleRotationArrowStatus, setToggleRotationArrowStatus] = useState<boolean[]>([]);
   const [isClicked, setIsClicked] = useState(false);
-  const [rotateArrow, setRotateArrow] = useState(false);
-  const [isCategoryOpen, setIsCategoryOpen] = useState(false);
-  
+
   const text = {...strings.playgroundPage};
-  const categoryObj = [...strings.playgroundPage.category];
-  
-  useEffect(() => {
-    initializeToggleArrowStatus();
-    initializetoggleCarouselStatus();
-    initializeRotationArrowStatus();
-  }, []);
-  
-  const initializeToggleArrowStatus = () => {
-    let tempArr: boolean[] = [...toggleArrowStatus];
-    categoryObj.map(() => {
-      tempArr.push(false);
-    },
-    setStatus(tempArr))
-  };
-  
-  const initializetoggleCarouselStatus = () => {
-    let tempArr: boolean[] = [...toggleCarouselStatus];
-    categoryObj.map(() => {
-      tempArr.push(false);
-    },
-    setToggleCarouselStatus(tempArr))
-  };
 
-  const initializeRotationArrowStatus = () => {
-    let tempArr: boolean[] = [...toggleRotationArrowStatus];
-    categoryObj.map(() => {
-      tempArr.push(false);
-    },
-    setToggleRotationArrowStatus(tempArr))
-  };
-  
-  const toggleArrow = (index: number) => {
-    let switchStat = [...toggleArrowStatus];
-    switchStat[index] = !switchStat[index];
-    setStatus(switchStat);
+  const isArrowClicked = () => {
+    setIsClicked(isClicked => !isClicked);
   }
   
-  const arrowOnHandleClick = () => {
-    setIsClicked(prev => !prev);
-  }
-  
-  const toggleCarousel = (index: number) => {
-    let switchStat = [...toggleCarouselStatus];
-    switchStat[index] = !switchStat[index];
-    setToggleCarouselStatus(switchStat);
-  };
-  
-  const carouselOnHandleClick = () => {
-    setIsCategoryOpen(isOpen => !isOpen);
-  }
-
-  const toggleRotationArrow = (index: number) => {
-    let switchStat = [...toggleRotationArrowStatus];
-    switchStat[index] = !switchStat[index];
-    setToggleRotationArrowStatus(switchStat);
-  };
-
-  const handleRotate = () => setRotateArrow(isRotateArrow => !isRotateArrow);
-
-  const rotate = (index: number) => toggleRotationArrowStatus[index] ? "rotate(-180deg)" : "rotate(0deg)";
-  
-  const categoryContentMapper = categoryObj.map( (element, index) => {
-    
-    const carouselMapper = element.carousel.map( (item, index) => {
-      console.log("el carousel map:", item)
-      return (
-        <>
-          <SwiperSlide key={index}>
-            <img 
-              className='carousel-img'
-              src={item.imagePath} 
-            />
-          </SwiperSlide>
-        </>
-      )
-    });
-
-    return (
-      <>
-      <h2 
-        className={toggleArrowStatus[index] ? 'playground-category-title-clicked' : 'playground-category-title'}
-        >
-          <span className={toggleArrowStatus[index] ? 'playground-category-subtitle-clicked' : 'playground-category-subtitle'}>
-            {element.subtitle}
-          </span>
-          {element.title}
-        <a 
-          key={index} 
-          type='button'
-          className='playground-category-btn'
-          onClick={() => {
-            toggleArrow(index);
-            toggleCarousel(index);
-            arrowOnHandleClick();
-          }}
-        >
-          <FontAwesomeIcon
-              key={element.index}
-              className="playground-category-arrow"
-              style={{transform: rotate(index), transition: "all 0.2s linear"}}
-              title="ArrowUp" 
-              icon={faArrowDown}
-              onClick={() => {
-                toggleRotationArrow(index)
-              }}
-            />
-        </a>
-      </h2>
-      { toggleCarouselStatus[index] ? 
-        (
-          <Swiper
-            slidesPerView={5}
-            spaceBetween={10}
-            centeredSlides={true}
-            pagination={{
-              clickable: true,
-            }}
-            breakpoints={{
-              "@0.00": {
-                slidesPerView: 1.5,
-                spaceBetween: 25,
-              },
-              "@0.75": {
-                slidesPerView: 2.5,
-                spaceBetween: 40,
-              },
-              "@0.90": {
-                slidesPerView: 3,
-                spaceBetween: 40,
-              },
-              "@1.00": {
-                slidesPerView: 4,
-                spaceBetween: 30,
-              },
-            }}
-            modules={[Pagination]}
-            >
-              {carouselMapper}
-          </Swiper>
-        ) : (
-          <HrLine />
-        )
-      }
-      </>
-    )
-  });
-
   return ( 
     <>
     <motion.main 
@@ -203,7 +50,7 @@ const Playground = () => {
                     </section>
                     <section className="playground-projects-scroll">
                       <div className="playground-project" data-active="on">
-                        {categoryContentMapper}
+                        <Carousel isArrowClicked={isArrowClicked}/>
                       </div>
                     </section>
                     <h1 className="playground-footer-title">
