40d07c62b4ec9fccc0a142add40716e2b88a0f7f
FranciscoGuedes <francisco.guedes@polarising.com>
2022-06-30 23:38:07 +0100

    css fix

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -135,6 +135,8 @@ const Carousel = ({ isArrowClicked }: any) => {
           <Swiper
             slidesPerView={5}
             spaceBetween={10}
+            loop={true}
+            loopFillGroupWithBlank={true}
             pagination={{
               clickable: true,
             }}
