ce42ffb10a84b3399ddce31f6d23800456f1d2c9
FranciscoGuedes <francisco.guedes@polarising.com>
2022-07-23 17:58:18 +0100

    changed wiggle props to object

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,28 +4,39 @@ import Wiggle from '../wiggle/wiggle';
 
 import './navbar.css';
 
-const labelText = {...functionalitiesAlias.navbar};
-
 const Navbar = () => {
+  const labelText = {...functionalitiesAlias.navbar};
+
+  const labelTextObject = { 
+    labelTextCv: labelText.cv, 
+    labelTextWork: labelText.work,
+    labelTextPlayground: labelText.playground
+  };
   return ( 
     <>
       <div className="navbar-wrapper">
         <h2 className='cv-title navbar-title'>
           <Link rel="canonical" data-new-state="cv" className='cv-title-link wiggle-link' to="https://drive.google.com/file/d/1VIzDJ8zL--0NaKThG0sTtc1Fys73_40W/view?usp=sharing" target="_blank" >
             {labelText.cv}
-            <Wiggle wordText={labelText.cv}/>
+            <Wiggle 
+              labelTextProps={labelTextObject}
+            />
           </Link>
         </h2>
         <h2 className='work-title navbar-title'>
           <Link rel="canonical" data-new-state="work" className='work-title-link wiggle-link' to="/work">
             {labelText.work}
-            <Wiggle wordText={labelText.work}/>
+            <Wiggle 
+              labelTextProps={labelTextObject}
+            />
           </Link>
         </h2>
         <h2 className='playground-title navbar-title'>
           <Link data-new-state="playground" className='playground-title-link wiggle-link' to="/playground" rel="canonical">
             {labelText.playground}
-            <Wiggle wordText={labelText.playground}/>
+            <Wiggle 
+              labelTextProps={labelTextObject}
+            />
           </Link>
         </h2>
       </div>
