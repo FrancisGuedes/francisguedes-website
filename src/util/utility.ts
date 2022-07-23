2e9d50804b6e800cb5899141dfb059ac71c53c72
FranciscoGuedes <francisco.guedes@polarising.com>
2022-07-23 17:57:15 +0100

    updated utility.js

--- src/util/utility.ts
+++ src/util/utility.ts
@@ -1,10 +1,14 @@
-export function createClassName(defaultClass: string, classes?: string) {
+export function createClassName(defaultClass: string, classes?: string): string {
   return classes ? `${classes} ${defaultClass}` : defaultClass;
 }
 
-export function getTextWidth(wordText: string, htmlTagId: string) {
-  const canvas: any = document.createElement(htmlTagId);
+export function getTextWidthInPixels(wordText: string): number {
+  const canvas: any = document.createElement('canvas');
   const context: any = canvas.getContext('2d');
   let text = context.measureText(wordText);
   return text.width;
+}
+
+export function convertPXToVW(px: number): number {
+  return +(px * (100 / document.documentElement.clientWidth)).toFixed(1)*2.5;
 }
