90b8c70122b130f734d99d88880c06be62fe5f54
FranciscoGuedes <francisco.guedes@polarising.com>
2022-08-30 23:20:38 +0100

    aaded new method to change bg color loadingscreen

--- src/components/loading-screen/loadingScreen.tsx
+++ src/components/loading-screen/loadingScreen.tsx
@@ -2,10 +2,17 @@ import { useEffect, useRef } from 'react';
 import Gif from './gif/gif';
 import './loadingScreen.css';
 import { useState } from 'react';
+import { colors } from '../../util/colors';
+import * as endpoint from '../../api/endpoints';
 
 const LoadingScreen = () => {
   const ref = useRef<HTMLInputElement>(null);
   const [opacity, setOpacity] = useState<number>(1);
+  const currentLocationUrl: string = window.location.pathname;
+  const urlHomeLocation: string = endpoint.urlHome;
+  const urlWhoLocation: string = endpoint.urlWho;
+  const urlWorkLocation: string = endpoint.urlWork;
+  const urlPlayingLocation: string = endpoint.urlPlaying;
 
   useEffect(() => {
     let elementStyleOpacity: string | undefined = ref.current?.style.opacity;
@@ -24,7 +31,29 @@ const LoadingScreen = () => {
       }
       elementStyleOpacity = elementStyleOpacityToNumber.toString()
     }, 1000);
+    changingLoadingScreenBackground()
   }, []);
+
+  function changingLoadingScreenBackground(): void {
+    const backgroundColor = {...colors.new_background};
+    switch(currentLocationUrl) {
+      case urlHomeLocation:
+        document.documentElement.style.setProperty('--background', backgroundColor.yellow);
+        break;
+      case urlWhoLocation:
+        document.documentElement.style.setProperty('--background', backgroundColor.blue);
+        break;
+      case urlWorkLocation:
+        document.documentElement.style.setProperty('--background', backgroundColor.purple);
+        break;
+      case urlPlayingLocation:
+        document.documentElement.style.setProperty('--background', backgroundColor.orange);
+        break;
+      default:
+        document.documentElement.style.setProperty('--background', backgroundColor.pink);
+        break;
+    }
+  };
   
   return ( 
     <>
